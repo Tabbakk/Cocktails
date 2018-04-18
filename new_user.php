178b206044295592344ece88ac042c868d0352aa
Tabbakk <Tabbakk@users.noreply.github.com>
2018-04-18 16:48:59 -0600

    CSS and db conn setup

---
+++ new_user.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <link href="css/general.css" rel="stylesheet">
+    <title>Create User</title>
+  </head>
+
+  <body>
+    <div class="main">
+    	<ul>
+    		<li><a href="index.php">Back</a></li>
+    	</ul>
+    </div>
+  </body>
+
+</html> 
