fcc24df7f1a3492e9c23af2188320fbde098908b
Tabbakk <Tabbakk@users.noreply.github.com>
2018-04-20 15:50:59 -0600

    Js separated
    separated js for generalizzation

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
   <body>
     <div class="main">
     	<ul>
-    		<li><a href="new_user.php">Create User</a></li>
+    		<li><a href="new_user.php">Login</a></li>
+    		<li><a href="TestScripts/DynamicForm.html">Tests</a></li>
     	</ul>
     </div>
   </body>
